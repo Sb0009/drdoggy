5643d7a259c1ff09b60333fc294807e65df451e8
Sb0009 <phenissia2009@hotmail.fr>
2022-10-14 19:10:18 +0200

    use puma via procfile

--- app/controllers/doctor_controller.rb
+++ app/controllers/doctor_controller.rb
@@ -1,23 +1,35 @@
 # frozen_string_literal: true
+class DoctorsController < ApplicationController
+  before_action :set_doctor, only: %i[ show edit update destroy ]
+  before_action :adminAuthorized, only: %i[ new edit create update destroy ]
 
-class DoctorController < ApplicationController
-  before_action :set_doctor, only: %i[show edit update destroy]
   # GET /doctors or /doctors.json
   def index
     @doctors = Doctor.all
+
+    if params[:city].present?
+      @doctors = @doctors.where(city_id: params[:city])
+    end
+
+    if params[:city].present?
+      @doctors = @doctors.where(city_id: params[:city])
+    end
+
   end
 
   # GET /doctors/1 or /doctors/1.json
-#   CORRECTION HERE ----- def show
-#     ; end
+  def show
+  end
 
   # GET /doctors/new
   def new
-    @doctor = Doctor.new
+    @doctors = Doctor.new
   end
 
   # GET /doctors/1/edit
-  def edit; end
+  def edit
+    @appointment = get_current_appointments(@doctor)
+  end
 
   # POST /doctors or /doctors.json
   def create
@@ -25,7 +37,7 @@ class DoctorController < ApplicationController
 
     respond_to do |format|
       if @doctor.save
-        format.html { redirect_to @doctor, notice: 'Doctor was successfully created.' }
+        format.html { redirect_to doctor_url(@doctor), notice: "Doctor was successfully created." }
         format.json { render :show, status: :created, location: @doctor }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +50,13 @@ class DoctorController < ApplicationController
   def update
     respond_to do |format|
       if @doctor.update(doctor_params)
-        format.html { redirect_to @doctor, notice: 'Doctor was successfully updated.' }
+        if @doctor.capacity == get_current_bookings(@doctor)
+          @doctor.status = "busy"
+        else
+          @doctor.status = "available"
+        end
+        @doctor.save
+        format.html { redirect_to doctor_url(@doctor), notice: "Doctor was successfully updated." }
         format.json { render :show, status: :ok, location: @doctor }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -48,27 +66,32 @@ class DoctorController < ApplicationController
   end
 
   # DELETE /doctors/1 or /doctors/1.json
-  def doctors_url
-    # code here
-  end
-
   def destroy
     @doctor.destroy
+
     respond_to do |format|
-      format.html { redirect_to doctors_url, notice: 'Doctor profile was successfully destroyed.' }
+      format.html { redirect_to doctors_url, notice: "Doctor profile was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-
   # Use callbacks to share common setup or constraints between actions.
   def set_doctor
-    @doctor = Doctor.find_by_id(params[:id])
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def get_current_bookings (doctor)
+    begin
+      return Appointment.where(doctor_id: doctor.id).map{|x| x.patients}.inject {|v, n| v+n}
+    rescue
+      return 0
+    end
   end
 
   # Only allow a list of trusted parameters through.
   def doctor_params
-    params.require(:doctor).permit(:fam_name, :content)
+    params.require(:doctor).permit(:id, :last_name, :city_id,
+                                   :speciality,  :status, :city, :city)
   end
 end
