dd20df095fb89ec5aaf203a6b8a6972e0e1b1214
Siham Badyine <96126445+Sb0009@users.noreply.github.com>
2022-10-20 12:54:52 +0200

    Update doctor.rb
    has_many :appointments
    	has_many :patients, through: :appointments

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,8 @@ class Doctor < ApplicationRecord
   belongs_to :specialty
 
   has_and_belongs_to_many :patients
-
+  has_many :appointments
+	has_many :patients, through: :appointments
   validates :name, presence: true
   validates :email, presence: true
 end
